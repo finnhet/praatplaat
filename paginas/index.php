740f73a69144f90c8dc04bc664df692bb409640a
finnhet <149392001+finnhet@users.noreply.github.com>
2024-03-13 11:11:52 +0100

    meow

--- paginas/index.php
+++ paginas/index.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple HTML Page</title>
+    <title>Praatplaat</title>
+    <?php
+    include '../hooters/header.php'
+    ?>
     <style>
         body {
             font-family: Arial, sans-serif;
