3bb71e2194b2914e855eb62ea037aa84530458bd
0mpie27 <144774774+0mpie27@users.noreply.github.com>
2024-03-19 09:14:26 +0100

    design klaar

--- elementEdit.php
+++ elementEdit.php
@@ -20,7 +20,7 @@
   justify-content: center;
   align-content: center;
   align-items: center;
-  margin-top: 20rem;
+  margin-top: 16rem;
 }
 
 .gap-1 {
@@ -31,7 +31,7 @@
   margin-bottom: 20px; /* Adjust this value to increase or decrease the gap */
 }
 
-.btn{
+.btn-dark{
     margin-top: 20px;
 }
 
@@ -72,8 +72,21 @@
       <div class="card card-body">
 
 
+      <select class="form-select input-gap" aria-label="Default select example">
+      <option selected>Selecteer een element</option>
+      <option value="1">brood</option>
+      <option value="2">ei</option>
+      <option value="3">kaas</option>
+      </select>
 
-        //
+      <input class="form-control input-gap" type="text" placeholder="Nieuwe Naam NL" aria-label="default input example">
+      <input class="form-control input-gap" type="text" placeholder="Nieuwe Naam FR" aria-label="default input example">
+      <input class="form-control input-gap" type="text" placeholder="Nieuwe Naam EN" aria-label="default input example">
+
+      <label for="formFile" class="form-label">Voeg een element toe.</label>
+      <input class="form-control" type="file" id="formFile">
+
+      <button class="btn btn-be btn-dark" type="submit">bewerken</button>
 
 
 
@@ -87,7 +100,37 @@
         
 
 
-      ..
+    <table class="table table-striped table-hover">
+      <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Naam</th>
+      <th scope="col">verwijder</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>brrod</td>
+      <td>  <button type="button" class="btn btn-outline-danger">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
+          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
+          </svg>
+        verwijder
+      </button> </td>
+    <tr>
+      <th scope="row">2</th>
+      <td>kaas</td>
+      <td>  <button type="button" class="btn btn-outline-danger">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
+          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
+          </svg>
+        verwijder
+      </button> </td>
+    </tbody>
+    </table>
 
 
       
