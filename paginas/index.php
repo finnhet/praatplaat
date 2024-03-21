6bc0d41dd697100252d1ed7fc1003f5558cd5cd9
bramus123 <251972@edu.rocfriesepoort.nl>
2024-03-21 15:59:32 +0100

    wip index.php (bootstrap)

--- paginas/index.php
+++ paginas/index.php
@@ -8,6 +8,8 @@
     include '../db.php';
     include '../extra/header.php'
     ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -27,7 +29,6 @@
             grid-gap: 20px;
         }
         h1 {
-            text-align: center;
             color: #333;
         }
         p {
