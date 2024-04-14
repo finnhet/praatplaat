c416ada2bf152e9bdbbf1d9f79898fdcef2536ac
tristytn <tristandevries07@gmail.com>
2024-04-14 17:11:56 +0200

    FINALE

--- paginas/elementen.php
+++ paginas/elementen.php
@@ -229,6 +229,13 @@ if (isset($_SESSION['username'])) {
         var modal = document.getElementById('myModal');
         modal.style.display = "none";
     }
+   // Add this function to handle clicks outside the modal content
+   window.onclick = function(event) {
+        var modal = document.getElementById('myModal');
+        if (event.target == modal) {
+            closeModal();
+        }
+    }
 </script>
 </body>
 </html>
